6bf6caa5e293afc55269afe42f8ffb51f2852ce9
Abby Suh <abbysuh@hotmail.com>
2018-07-02 20:37:00 -0700

    Day 1 Notes and examples.

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+/* DECLARE YOUR VARIABLE
+    camelCasing is the best practice is JavaScript
+    Always end a JavaScript statement with a semi-colon
+    Once you declare a variable, you need to define it
+    Make variable names as descriptive as you can, but not really long
+    */
+
+var firstName;
+
+/* DEFINE YOUR VARIABLE
+    Anything in quotes - "" '', is a string
+*/
+
+firstName = "Abby";
+
+// DEFINE AND DECLARE YOUR VARIABLE AT THE SAME TIME
+
+var lastName = "McLallin";
+
+/* TEST YOUR CODE IN THE CONSOLE!
+    If you put an error in the codes, the code will immediately stop.
+    First check variable names, then check to see if everything is in the right order.
+*/
+
+console.log(firstName, lastName);
+
+//REDEFINING THE VARIABLE
+
+firstName = "Superman"; //Keep quotations consistent "" or ''
+console.log(firstName);
+
+// Single line of code is //, no closing //.
+/* Multi-line comments use
+an asterisk, slash and closed with it */
+
+/*Scope indenting - one tab spacings
+  When you see curly brackets, 9/10 you are creating a new scope
+  Best of practice, will not break the code, but its important for reabiability
+*/
