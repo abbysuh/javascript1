c93912be77791648ecd574d8c94c07b9f8a037de
Abby Suh <abbysuh@hotmail.com>
2018-07-03 12:48:43 -0700

    Finished Day1 takehome

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,52 @@
-/* DECLARE YOUR VARIABLE
-    camelCasing is the best practice is JavaScript
-    Always end a JavaScript statement with a semi-colon
-    Once you declare a variable, you need to define it
-    Make variable names as descriptive as you can, but not really long
-    */
+// DAY 1 NOTES AND EXAMPLES
+//
+// /* DECLARE YOUR VARIABLE
+//     camelCasing is the best practice is JavaScript
+//     Always end a JavaScript statement with a semi-colon
+//     Once you declare a variable, you need to define it
+//     Make variable names as descriptive as you can, but not really long
+//     */
+//
+// var firstName;
+//
+// /* DEFINE YOUR VARIABLE
+//     Anything in quotes - "" '', is a string
+// */
+//
+// firstName = "Abby";
+//
+// // DEFINE AND DECLARE YOUR VARIABLE AT THE SAME TIME
+//
+// var lastName = "McLallin";
+//
+// /* TEST YOUR CODE IN THE CONSOLE!
+//     If you put an error in the codes, the code will immediately stop.
+//     First check variable names, then check to see if everything is in the right order.
+// */
+//
+// console.log(firstName, lastName);
+//
+// //REDEFINING THE VARIABLE
+//
+// firstName = "Superman"; //Keep quotations consistent "" or ''
+// console.log(firstName);
+//
+// // Single line of code is //, no closing //.
+// /* Multi-line comments use
+// an asterisk, slash and closed with it */
+//
+// /*Scope indenting - one tab spacings
+//   When you see curly brackets, 9/10 you are creating a new scope
+//   Best of practice, will not break the code, but its important for reabiability
+// */
 
-var firstName;
 
-/* DEFINE YOUR VARIABLE
-    Anything in quotes - "" '', is a string
-*/
+//TAKE HOME - DAY 1
 
-firstName = "Abby";
+var answer1 = "A working understanding of JavaScript so that I can read other people's code as well as implement it into my own work. I'd also like to further my knowledge and learn new skills in coding and tech.";
 
-// DEFINE AND DECLARE YOUR VARIABLE AT THE SAME TIME
+console.log (answer1);
 
-var lastName = "McLallin";
+var answer2 = "I really enjoy gaming - PC gaming as well as Warmachine, board games, and Magic: The Gathering. Outside of that I really enjoy traveling to new places and trying new restaurants or exotic food."
 
-/* TEST YOUR CODE IN THE CONSOLE!
-    If you put an error in the codes, the code will immediately stop.
-    First check variable names, then check to see if everything is in the right order.
-*/
-
-console.log(firstName, lastName);
-
-//REDEFINING THE VARIABLE
-
-firstName = "Superman"; //Keep quotations consistent "" or ''
-console.log(firstName);
-
-// Single line of code is //, no closing //.
-/* Multi-line comments use
-an asterisk, slash and closed with it */
-
-/*Scope indenting - one tab spacings
-  When you see curly brackets, 9/10 you are creating a new scope
-  Best of practice, will not break the code, but its important for reabiability
-*/
+console.log (answer2);
