75d1c04ffb4f4f4b4906dd52d3b38ce84f5f4817
Abby Suh <abbysuh@hotmail.com>
2018-07-05 17:56:21 -0700

    Changes July 5

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,6 @@
-// DAY 1 NOTES AND EXAMPLES
-//
-// /* DECLARE YOUR VARIABLE
-//     camelCasing is the best practice is JavaScript
-//     Always end a JavaScript statement with a semi-colon
-//     Once you declare a variable, you need to define it
-//     Make variable names as descriptive as you can, but not really long
-//     */
-//
-// var firstName;
-//
-// /* DEFINE YOUR VARIABLE
-//     Anything in quotes - "" '', is a string
-// */
-//
-// firstName = "Abby";
-//
-// // DEFINE AND DECLARE YOUR VARIABLE AT THE SAME TIME
-//
-// var lastName = "McLallin";
-//
-// /* TEST YOUR CODE IN THE CONSOLE!
-//     If you put an error in the codes, the code will immediately stop.
-//     First check variable names, then check to see if everything is in the right order.
-// */
-//
-// console.log(firstName, lastName);
-//
-// //REDEFINING THE VARIABLE
-//
-// firstName = "Superman"; //Keep quotations consistent "" or ''
-// console.log(firstName);
-//
-// // Single line of code is //, no closing //.
-// /* Multi-line comments use
-// an asterisk, slash and closed with it */
-//
-// /*Scope indenting - one tab spacings
-//   When you see curly brackets, 9/10 you are creating a new scope
-//   Best of practice, will not break the code, but its important for reabiability
-// */
-
-
 //TAKE HOME - DAY 1
+
+// Instructions : Write some JavaScript that logs a message to the console describing what you want to get out of this class and your favorite hobby.
 
 var answer1 = "A working understanding of JavaScript so that I can read other people's code as well as implement it into my own work. I'd also like to further my knowledge and learn new skills in coding and tech.";
 
