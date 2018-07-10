76ce5e201a6a535eb89a1e76c8160626fdc2822d
Abby Suh McLallin <37916073+abbysuh@users.noreply.github.com>
2018-07-09 19:09:56 -0700

    Update scripts.js
    Changing operators and comparison.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
- let firstName = prompt("What is your first name?", "John");
+let firstName = prompt("What is your first name?", "John");
 
 let lastName = prompt("What is your last name?", "Doe");
 
 console.log(firstName);
 
-if (lastName == null | lastName == "") {
+if (lastName === null || lastName === "") {
   confirm("Are you sure you don't want to enter your last name?");
 } else {
   alert(`Your last name is ${lastName}. What a neat last name!`);
@@ -17,7 +17,7 @@ let birthday = prompt("What is your birthday?", "2/7/1994");
 
 let confirmBirthday = confirm(`Are you sure your birthday is ${new Date(birthday)}?`);
 
-if (confirmBirthday == false) {
+if (confirmBirthday === false) {
   alert("Butter fingers I'm guessing?")
 } else {
   alert(`Your birthday is ${new Date(birthday)}.`);
