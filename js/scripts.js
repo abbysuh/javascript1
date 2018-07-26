0c0caff887e0ba4b79a20ab3c41e49a0b489e6d2
Abby Suh <abbysuh@hotmail.com>
2018-07-25 20:59:18 -0700

    Start of day 8 notes.

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// document.forms[1]
+
+// let loginForm = document.forms[0];
+
+// document.forms.(name)
+
+let loginForm = document.forms.login;
+
+// console.log(document.forms,loginForm);
+
+let emailImput = loginForm.email;
+
+// FE gives data to the backend and receives the data back to print on the page.
+
+// ? = after is a query parameter.
+
+// & = separating the different parameters.
