4ac66f662bfba47c18ff0f395540657bf0d0629f
Abby Suh <abbysuh@hotmail.com>
2018-07-30 19:58:16 -0700

    this.

--- js/scripts.js
+++ js/scripts.js
@@ -19,8 +19,61 @@ let emailImput = loginForm.email;
 let passwordInput = loginForm.password;
 
 emailImput.addEventListener("keyup", e => {
-  console.log(e.keyCode); // What key did they hit?
-  e.keyCode; // What is getting typed in.
-})
+  listItems.call(emailImput);
+  // e.keyCode; // What is getting typed in.
+  console.log(this);
+});
 
 // RegEx — Regular expression, use to see if their input matches a pattern.
+
+loginForm.addEventListener("submit", e => { // Submit event type, only works on forms.
+  e.preventDefault(); // Stops default action of event.
+  // ... Do some checks, submit the form, etc.
+  // console.log(e);
+  loginForm.reset(); // Clears inputs on forms.
+});
+
+// THIS
+
+// Property of a function, doesn't get defined until the function is called.
+
+// Refer to the context in which the function is called - this inside of a function.
+
+// If you don't change the definition of this in the function it will always be the window (global scope).
+// let favorites = ["pickles"];
+function listItems(pet) {
+  // console.log(this);
+  // alert (`${this.name} loves ${this.favorites[0]} and has a ${pet}.`);
+};
+
+// listItems();
+
+let user = {
+  name: "Abby",
+  cart: [],
+  coupon: [],
+  favorites: ["the Void"],
+  listFavorites: listItems // Implicitly binds this
+};
+
+// Callstack — The way the engine inside the browser handles all the different things you feed it with JavaScript.
+
+// Implicit "this" Binding
+
+user.listFavorites();
+// log out : Ryeker loves cheese.
+
+// Explicit Binding
+
+let jr = {
+  name: "Junior Royce?",
+  cart: ["Guns N' Roses"],
+  coupons: [1.99],
+  favorites: ["Star Wars"]
+};
+
+// Call
+// listItems.call(jr, "dog"); // Rule 1 about call is that it must be called on a function. Rule 2 is if the functiob beinf bound requires arguments, they begin as the 2nd arguments of the call.
+
+// Apply
+// listItems.apply(jr,["cat"]); // 2nd argument is in an array.
